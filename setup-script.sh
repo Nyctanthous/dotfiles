89cb94e84e5ab994f13c833b103be081d6068a6f
Ben Montgomery <ben_mon@outlook.com>
2019-07-11 02:48:29 -0400

    Updated setup script

--- setup-script.sh
+++ setup-script.sh
@@ -16,29 +16,39 @@ makepkg -fsri
 # Base requirements
 REQ[0]="jsoncpp"     # To help with Polybar
 REQ[1]="xclip"       # Helps with copy & paste.
-REQ[2]="most"        # For paging, with color!
-REQ[3]="feh"         # Wallpaper setter
-REQ[4]="w3m"         # In-console image viewer
-REQ[5]="highlight"   # Text highlighter you can pipe into
-REQ[6]="hblock"      # System-level ad domain blocking.
-REQ[7]="exa"         # Better implementation of LS, written in Rust.
-REQ[8]="gotop"       # Replacement to top, written in Go.
-REQ[9]="pywal"       # Color theme generation.
+REQ[2]="highlight"   # Text highlighter you can pipe into
+
+# Windowing
+REQ[3]="i3-gaps"     # Window manager
+REQ[4]="compton"     # Compositor
+
+# Theming Applications
+REQ[5]="pywal"       # Color theme generation.
+REQ[6]="wpgtk-git"   # Color theme generation for GTK; uses Pywal
+
+# Misc Applications
+REQ[7]="hblock"      # System-level ad domain blocking.
+REQ[8]="w3m"         # In-console image viewer
+REQ[9]="feh"         # Wallpaper setter
+REQ[10]="redshift"   # Bluelight filter
 
 # Terminal Applications
-REQ[9]="ranger"      # Terminal file manager
+REQ[11]="ranger"     # Terminal file manager
+REQ[12]="gotop"      # Replacement to top, written in Go.
+REQ[13]="exa"        # Better implementation of LS, written in Rust.
+REQ[14]="most"       # For paging, with color!
+REQ[15]="neofetch"   # System info in the terminal
 
 # GTK/QT Applications
-REQ[11]="deadbeef"   # Audiophile music player.
-REQ[11]="vscodium"   # IDE with Microsoft's binaries removed.
-REQ[11]="firefox"    # Web browser, of course.
+REQ[16]="vscodium"   # IDE with Microsoft's binaries removed.
+REQ[17]="firefox"    # Web browser, of course.
 
 # Major requirements
-REQ[5]="i3-gaps"     # Window manager
-REQ[6]="alacritty"   # GPU-driven terminal
-REQ[7]="rofi"        # App launcher
-REQ[8]="mksh"        # Shell
-REQ[10]="polybar"    # Bar
+REQ[18]="kitty-git"  # GPU-driven terminal
+REQ[19]="rofi"       # App launcher
+REQ[20]="zsh"        # Shell
+REQ[21]="oh-my-zsh"  # Shell extensions
+REQ[22]="polybar"    # Bar
 
 
 # Actually install the packages
